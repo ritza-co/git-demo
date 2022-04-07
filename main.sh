68747f7530e47478308fdb336da5bf5d23c7d124
DanielleMurray <>
2022-04-07 14:07:08 +0000

    Update README.md

--- main.sh
+++ main.sh
@@ -1,10 +1,11 @@
 git clone https://github.com/ritza-co/git-demo
 git add README.md
 git commit -m "Added the title of my tutorial with commands that will be featured in the article"
-ssh-keygen -t rsa -C "https://github.com/ritza-co/git-demo"
-cat ~/.ssh/id_rsa.pub | clip
+ssh-keygen -t rsa -b 4096 -C "danielle@ritza.co"
+cat ~/.ssh/id_rsa.pub 
 ssh -T git@github.com
 git remote set-url origin git@github.com:username/https://github.com/ritza-co/git-demo
 git add README.md
 git commit -am "Update README.md"
 git push
+
